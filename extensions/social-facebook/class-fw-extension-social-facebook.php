9852e50c698cf2bd5cebd30f66f9e93636fa5f79
Valeriu <valeriuzdrobau@gmail.com>
2014-12-11 18:39:11 +0200

    added method to Accessing Facebook Graph API

--- extensions/social-facebook/class-fw-extension-social-facebook.php
+++ extensions/social-facebook/class-fw-extension-social-facebook.php
@@ -4,15 +4,53 @@
 
 class FW_Extension_Social_Facebook extends FW_Extension {
 
+	private $access_token_name = 'access_token';
+
+	private $access_token;
+
 	/**
 	 * @internal
 	 */
 	public function _init() {
+		$this->add_actions();
 		$this->add_filters();
+	}
+
+	private function add_actions() {
+		add_action( 'fw_extension_settings_form_saved', array(
+				$this,
+				'_action_fw_ext_social_facebook_save_options'
+			), 10, 2 );
 	}
 
 	private function add_filters() {
 		add_filter( 'fw_ext_social_tabs', array( $this, '_filter_fw_ext_social_tabs' ) );
+	}
+
+	/**
+	 * @param $extension_name
+	 * @param $options_before_save
+	 */
+	public function _action_fw_ext_social_facebook_save_options( $extension_name, $options_before_save ) {
+		$parent = $this->get_parent()->get_name();
+
+		if ( $extension_name === $parent ) {
+
+			$response = wp_remote_get(
+				add_query_arg(
+					array(
+						'grant_type'    => 'client_credentials',
+						'client_id'     => fw_get_db_ext_settings_option( $parent, 'facebook-app-id' ),
+						'client_secret' => fw_get_db_ext_settings_option( $parent, 'facebook-app-secret' )
+					), 'https://graph.facebook.com/oauth/access_token' ) );
+			$body     = wp_remote_retrieve_body( $response );
+			if ( strpos( $body, 'access_token=' ) !== false ) {
+				$token = substr( $body, 13 );
+				fw_set_db_extension_data( $this->get_name(), $this->access_token_name, $token );
+			} else {
+				fw_set_db_extension_data( $this->get_name(), $this->access_token_name, false );
+			}
+		}
 	}
 
 	/**
@@ -54,5 +92,51 @@ class FW_Extension_Social_Facebook extends FW_Extension {
 		);
 
 		return array_merge( $tabs, $facebook_tab );
+	}
+
+	/**
+	 * Accessing Facebook Graph API
+	 *
+	 * @param string $method
+	 * @param $node
+	 * @param $args
+	 * @param bool $token
+	 * @param string $version
+	 * @param string $graph_url
+	 *
+	 * @return string
+	 */
+	public function graph_api_explorer( $method = 'GET', $node, $args, $token = false, $version = 'v2.2', $graph_url = 'https://graph.facebook.com' ) {
+
+		if ( ! $token ) {
+			$token = $this->get_access_token();
+		}
+
+		$args = array_merge( (array) $args, array( 'access_token' => $token ) );
+
+		$req_url = $graph_url . '/' . $version . '/' . $node;
+
+		$req_url = ( $method === 'GET' ) ? add_query_arg( $args, $req_url ) : $req_url;
+
+		$response = wp_remote_request( $req_url, array(
+				'method'    => $method,
+				'sslverify' => false,
+				'body'      => ( $method !== 'GET' ) ? $args : null
+			)
+		);
+
+		return wp_remote_retrieve_body( $response );
+	}
+
+	/**
+	 * Return Access Token generated from App ID and App Secret, that you can use to display the posts from any public Facebook page or open group.
+	 * @return mixed|null
+	 */
+	public function get_access_token() {
+		if ( $this->access_token === null ) {
+			$this->access_token = fw_get_db_extension_data( $this->get_name(), $this->access_token_name );
+		}
+
+		return $this->access_token;
 	}
 }

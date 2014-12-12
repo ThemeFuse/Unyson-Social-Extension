9a9ff781f3bef8ed0bc25197462fd35fabd4e153
Valeriu <valeriuzdrobau@gmail.com>
2014-12-12 18:08:42 +0200

    added description in manifest.php

--- manifest.php
+++ manifest.php
@@ -5,7 +5,7 @@
 $manifest = array();
 
 $manifest['name']        = __( 'Social', 'fw' );
-$manifest['description'] = __( 'Social', 'fw' );
+$manifest['description'] = __( 'This extension offers the opportunity to work with social networks.', 'fw' );
 $manifest['version'] = '1.0.0';
 $manifest['display'] = true;
 $manifest['standalone'] = true;

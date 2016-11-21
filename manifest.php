e39abb1ef548944b71057bc78bed2f1205cc040e
moldcraft <moldcraft@email.com>
2016-11-21 18:44:33 +0200

    Version 1.0.6

--- manifest.php
+++ manifest.php
@@ -6,7 +6,7 @@ $manifest = array();
 
 $manifest['name']        = __( 'Social', 'fw' );
 $manifest['description'] = __( 'Use this extension to configure all your social related APIs. Other extensions will use the Social extension to connect to your social accounts.', 'fw' );
-$manifest['version'] = '1.0.5';
+$manifest['version'] = '1.0.6';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
 $manifest['github_update'] = 'ThemeFuse/Unyson-Social-Extension';
